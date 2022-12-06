0cc0d2176d0be1bbedd4334b499b278e510f157c
Frank Wylupek <frankw@tuta.io>
2022-12-05 20:51:58 -0500

    add day 5, part 1 solution

--- day_05/part_01/part_01.py
+++ day_05/part_01/part_01.py
@@ -1,41 +1,46 @@
-# copy all lines temporarily into lists, longest set of '[' sets width of 
-# multi dimensional array, number of lines sets height
-
+# load input file and create a multidimensional array, with rows representing
+# layers of crates, and columns representing stacks
 def load_input(stacks_matrix: list, moves_list: list):
     input_list = []
-    open_file = open('example_input.txt')
+    open_file = open('input.txt')
     for line in open_file:
         input_list.append(line)
     open_file.close()
 
     stacks_list = []
     for line in input_list:
+        # use the left bracket as an indicator that the line contains a crate
         if '[' not in line:
             break
         else:
             stacks_list.append(line.strip('\n'))
     
+    # word 'move' indicates that this line is a move command
     for line in input_list:
         if 'move' in line:
             moves_list.append(line.strip())
     
     stacks_number = 0
     for stack in stacks_list:
+        # find highest number of stacks, the rows in the matrix
         if len(stack.split('[')) > stacks_number:
             stacks_number = len(stack.split('['))
     
     for line in stacks_list:
         temp_list = []
         for num in range(stacks_number - 1):
+            # every 4 characters, including empty space and brackets, denotes a
+            # new crate label, so slice the lines up every 4 characters, remove
+            # junk in lines afterward
             temp_list.append(line[num * 4:(num * 4) + 4].strip(' \n[]'))
         stacks_matrix.append(temp_list)
 
 def rearrange_crates(stacks_matrix: list, moves_list: list):
-    # indexes:
+    # indexes in move command after splitting:
     #   1 for how many
     #   3 is from
     #   5 is to
-    #   from and to are -1 because indexes start at 0, not 1
+    #   from and to have 1 subtracted because indexes start at 0, not 1
     how_many = []
     from_stack = []
     to_stack = []
@@ -43,45 +48,45 @@ def rearrange_crates(stacks_matrix: list, moves_list: list):
         how_many.append(int(move.split(' ')[1]))
         from_stack.append(int(move.split(' ')[3]) - 1)
         to_stack.append(int(move.split(' ')[5]) - 1)
+    
     # for each move
     for move in range(len(moves_list)):
         # for number of crates to move
         for count in range(how_many[move]):
-            # remove top layer if empty
+            # remove top layer from matrix if empty
             if stacks_matrix[0] == [''] * len(stacks_matrix[0]):
-                print('removing empty top layer')
                 stacks_matrix.pop(0)
-            print()
-            print(stacks_matrix)
-            print('move ' + str(move + 1) + ', operation ' + str(count + 1) + \
-                ' of ' + str(how_many[move]))
-            # for each vertical position in the stack
             is_complete = False
             crate_letter = ''
+            # for each vertical position in the stack
             for layer in range(len(stacks_matrix)):
                 if is_complete:
                     break
-                # skip layer if move from position is empty
+                # move to next layer if this 'move from' position is empty
                 if stacks_matrix[layer][from_stack[move]] != '':
-                    print(stacks_matrix[layer][from_stack[move]])
+                    # letter of crate being moved is first occupied
+                    # vertical space
                     crate_letter = stacks_matrix[layer][from_stack[move]]
-                    print('moving from ' + str(from_stack[move] + 1))
-                    print('moving to ' + str(to_stack[move] + 1))
+                    # lowest vertical position this crate can be moved to
+                    # as per the 'move to' part of the command
                     lowest_position = \
                         first_lowest_position(stacks_matrix, to_stack[move])
-                    print('lowest position ' + str(lowest_position))
-                    # remove crate from position
+                    # if there exists a space for the crate, move it there
                     if lowest_position > -1:
                         stacks_matrix[lowest_position][to_stack[move]] = \
-                            stacks_matrix[layer][from_stack[move]]
+                            crate_letter
                         stacks_matrix[layer][from_stack[move]] = ''
                         is_complete = True
+                    # if there is no space in this position
                     else:
                         # make a new layer
-                        print('adding empty top layer')
-                        stacks_matrix.insert(0, [''] * len(stacks_matrix[layer]))
+                        stacks_matrix.insert(0, \
+                            [''] * len(stacks_matrix[layer]))
+                        # add crate to desired position in new layer
                         stacks_matrix[0][to_stack[move]] = \
                             crate_letter
+                        # remove crate from old layer, plus 1 because a new
+                        # layer has been added since
                         stacks_matrix[layer + 1][from_stack[move]] = ''
                         is_complete = True
 
@@ -92,17 +97,24 @@ def first_lowest_position(stacks_matrix: list, to_position: int):
         elif reversed_layer == 0:
             return -1
 
+def format_results(stacks_matrix: list):
+    output_format = ''
+    temp_list = [''] * len(stacks_matrix[0])
+    for line in stacks_matrix:
+        for i in range(len(line)):
+            if line[i] != '' and temp_list[i] == '':
+                temp_list[i] = line[i]
+    for crate in temp_list:
+        output_format += crate
+    print(output_format)
+
 def main():
-    for num in range(5):
-        print('------------ BEGIN ------------')
     stacks_matrix = []
     moves_list = []
     load_input(stacks_matrix, moves_list)
     rearrange_crates(stacks_matrix, moves_list)
-
-    print('stacks result:')
-    print(stacks_matrix)
-    for num in range(5):
-        print('------------ END ------------')
+    for layer in stacks_matrix:
+        print(layer)
+    format_results(stacks_matrix)
 
 main()
