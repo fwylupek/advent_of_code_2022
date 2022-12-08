70e799f0eb02bf574e54571c8cd7bd42eac4ca91
Frank Wylupek <fwylupek@icloud.com>
2022-12-08 11:20:53 -0500

    add day 8, part 1 prompt, inputs, and idea

---
+++ day_08/part_01/part_01.py
@@ -0,0 +1,5 @@
+# load tree heights into tree_heights[][]
+# all trees in first and last rows, and first and last columns are visible
+# for each tree, it is visible if
+# directions above, below, left, or right are smaller
+# MAX_HEIGHT is 9
