442ce475a2eb938fe91dde7fbfb37a7798dc2eb9
Frank Wylupek <frankw@tuta.io>
2022-12-09 01:15:51 -0500

    add solution to day 8, part 2

--- day_08/part_02/part_02.py
+++ day_08/part_02/part_02.py
@@ -8,31 +8,10 @@ def load_input(input_filename: str, input_list: list):
 def load_tree_heights(input_list: list, tree_heights: list):
     for line in input_list:
         temp_list = []
-        for tree in line:
-            temp_list.append(tree)
+        for tree_height in line:
+            temp_list.append(int(tree_height))
         tree_heights.append(temp_list)
     return
-
-# return true if visible from any angle
-def get_visibility(x: int, y: int, tree_heights: list):
-    # return true if 'edge'
-    if x == 0 or x == len(tree_heights[0]) - 1:
-        return True
-    if y == 0 or y == len(tree_heights) - 1:
-        return True
-    # if the tree is bigger than any other tree in any direction, return true
-    if \
-    check_up(x, y, tree_heights) or \
-    check_down(x, y, tree_heights) or \
-    check_left(x, y, tree_heights) or \
-    check_right(x, y, tree_heights):
-        return True
-
-    # return false if a tree is blocking from any angle
-    return False
-
-# to change check* functions, start at row, column of tree, check trees in the
-# order away from tree until one tree is >= tree in question
 
 # get visibility in the up direction, go in reverse from the position of the
 # tree - 1
@@ -44,43 +23,77 @@ def check_up_visibility(row: int, column: int, tree_heights: list):
         if tree_heights[up_direction][row] >= tree_heights[column][row]:
             # return the viewing distance
             return view_distance
+        # if view reached the end of the tree line
         if up_direction == 0:
             # return the maximum viewing distance
             return view_distance
         view_distance += 1
 
-# get visibility in the down direction
+# get visibility in the down direction, ascending from tree
 def check_down_visibility(row: int, column: int, tree_heights: list):
-    # same as check_up(), just in reverse order
-    for down_direction in reversed(range(len(tree_heights))):
+    # same as check_up_visibility(), just in reverse order
+    view_distance = 1
+    # iterate through columns, ascending from current tree
+    for down_direction in range(column + 1, len(tree_heights)):
+        # blocking tree was found
         if tree_heights[down_direction][row] >= tree_heights[column][row]:
-            if down_direction == column:
-                return True
+            # return the viewing distance
+            return view_distance
+        # if view reached the end of the tree line
+        if down_direction == len(tree_heights) - 1:
+            # return the maximum viewing distance
+            return view_distance
+        view_distance += 1
 
-# get visibility in the left direction
+# get visibility in the left direction, descending from tree
 def check_left_visibility(row: int, column: int, tree_heights: list):
-    # iterate through rows, length of an index in overall array
-    for left_direction in range(len(tree_heights[0])):
+    view_distance = 1
+    # iterate through rows, descending from current tree
+    for left_direction in reversed(range(row)):
         # blocking tree was found
         if tree_heights[column][left_direction] >= tree_heights[column][row]:
-            # if the tree was itself, then all trees from this direciton
-            # have been checked
-            if left_direction == row:
-                return True
+            # return the viewing distance
+            return view_distance
+        # if view reached the end of the tree line
+        if left_direction == 0:
+            # return the maximum viewing distance
+            return view_distance
+        view_distance += 1
 
-# get visibility in the right direction
+# get visibility in the right direction, ascending from tree
 def check_right_visibility(row: int, column: int, tree_heights: list):
-    # same as check_left(), just in reverse order
-    for right_direction in reversed(range(len(tree_heights[0]))):
+    # same as check_left_visibility(), just in reverse order
+    view_distance = 1
+    # iterate through rows, ascending from current tree
+    for right_direction in range(row + 1, len(tree_heights[0])):
+        # blocking tree was found
         if tree_heights[column][right_direction] >= tree_heights[column][row]:
-            if right_direction == row:
-                return True
+            # return the viewing distance
+            return view_distance
+        # if view reached the end of the tree line
+        if right_direction == len(tree_heights[0]) - 1:
+            # return the maximum viewing distance
+            return view_distance
+        view_distance += 1
 
 def get_scenic_score(row: int, column: int, tree_heights: list):
     # look in every direction for how many trees are seen before the view
     # is blocked
-    # multiply the four view distances together to get the scenic score
     scenic_score = 0
+    # since one zero will make the whole score zero, any tree on the edge will
+    # have a scenic score of zero
+    if row == 0 or row == len(tree_heights[0]) - 1:
+        return scenic_score
+    if column == 0 or column == len(tree_heights)- 1:
+        return scenic_score
+    
+    # multiply the four view distances together to get the scenic score
+    scenic_score = \
+        check_up_visibility(row, column, tree_heights) * \
+        check_down_visibility(row, column, tree_heights) * \
+        check_left_visibility(row, column, tree_heights) * \
+        check_right_visibility(row, column, tree_heights)
+
     return scenic_score
 
 def main():
@@ -89,12 +102,15 @@ def main():
     input_list = []
     scenic_scores = []
 
-    load_input('example_input.txt', input_list)
+    load_input('input.txt', input_list)
     load_tree_heights(input_list, tree_heights)
+
+    for row in range(len(tree_heights)):
+        for column in range(len(tree_heights[0])):
+            scenic_scores.append(get_scenic_score(row, column, tree_heights))
 
     scenic_scores.sort()
 
-    #print('highest scenic score:', scenic_scores[-1])
-    print(check_up_visibility(1, 2, tree_heights))
+    print('highest scenic score:', scenic_scores[-1])
 
 main()
