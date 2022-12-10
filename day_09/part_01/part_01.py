7268cd49c1083f7a7f3fab30295951f82160cce6
Frank Wylupek <frankw@tuta.io>
2022-12-09 21:10:15 -0500

    add visualizer that accounts for negative values in grid

--- day_09/part_01/part_01.py
+++ day_09/part_01/part_01.py
@@ -67,43 +67,55 @@ def get_tail_positions(head_positions: list):
     return tail_positions
 
 def get_grid_size(head_positions: list):
-    width = 0
-    height = 0
+    grid_size = \
+        {'width': 0, 'height': 0, \
+        'min_x': 0, 'max_x': 0, \
+        'min_y': 0, 'max_y': 0}
 
     for position in head_positions:
-        if position[0] > width:
-            width = position[0]
-        if position[1] > height:
-            height = position[1]
+        if position[0] < grid_size['min_x']:
+            grid_size['min_x'] = position[0]
+        if position[0] > grid_size['max_x']:
+            grid_size['max_x'] = position[0]
+        if position[1] < grid_size['min_y']:
+            grid_size['min_y'] = position[1]
+        if position[1] > grid_size['max_y']:
+            grid_size['max_y'] = position[1]
+    
+    grid_size['width'] = abs(grid_size['min_x']) + abs(grid_size['max_x'])
+    grid_size['height'] = abs(grid_size['min_y']) + abs(grid_size['max_y'])
     
     # include [0, 0] position
-    width += 1
-    height += 1
+    grid_size['width'] += 1
+    grid_size['height'] += 1
     
-    print('grid size:', width, 'x', height)
-    return [width, height]
+    print('grid size:', grid_size['width'], 'x', grid_size['height'])
+    return grid_size
 
-def visualize_grid(positions: list, grid_size: list):
+# idk this will probably freeze your console with full input
+def visualize_grid(positions: list, grid_size: dict):
     # make empty grid
     grid = []
-
-    for height in range(grid_size[1]):
-        grid.append(['.'] * grid_size[0])
+    for height in range(grid_size['height']):
+        grid.append(['.'] * grid_size['width'])
     
     # replace coordinate in grid with move number
     for position in range(len(positions)):
-        grid[positions[position][1]][positions[position][0]] = position + 1
+        row = positions[position][0] + abs(grid_size['min_x'])
+        column = positions[position][1] + abs(grid_size['min_y'])
+        grid[column][row] = position + 1
         # print grid in reverse, so y=0 is on the bottom
-        if system('clear') != 0:
-            system('cls')
-        for line in reversed(range(len(grid))):
-            print(*grid[line])
-        sleep(0.7)
+#        if system('clear') != 0:
+#            system('cls')
+#        for line in reversed(range(len(grid))):
+#            print(*grid[line], sep='\t')
+#        sleep(0.7)
 
 def main():
     input_list = []
     head_positions = []
-    load_input('example_input.txt', input_list)
+    load_input('input.txt', input_list)
+    #load_input('example_input.txt', input_list)
     head_positions = get_head_positions(input_list)
     visualize_grid(head_positions, get_grid_size(head_positions))
 
